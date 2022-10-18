fa44229a3154c7c4a77dbac546e46771193dec3c
Camilo Andrés Rodríguez Garzón <cam_rod@hotmail.com>
2022-10-17 22:07:06 -0500

    [update Either object]

--- domain/src/main/java/com/buildreams/themovies/domain/model/action/Either.kt
+++ domain/src/main/java/com/buildreams/themovies/domain/model/action/Either.kt
@@ -1,22 +1,6 @@
 package com.buildreams.themovies.domain.model.action
 
 
-/*sealed interface Either {
-    data class Success(private val data: Any) : Either {
-        fun <R> getData() = data as R
-    }
-
-    data class Error(val error: ErrorEntity) : Either
-}*/
-
-//sealed interface EitherBeta {
-//    data class Error<E>(val error: E) : EitherBeta
-//    data class Success<R>(val value: R) : EitherBeta}
-//
-//    inline fun <reified T : Any> EitherBeta.success() = value
-//}
-
-
 sealed interface Either<out R, out E> {
     data class Success<R>(val data: R) : Either<R, Nothing>
 
